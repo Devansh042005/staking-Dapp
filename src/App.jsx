c1e0beef8b131b8cba2208b3d780df82555929f5
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-02-26 10:55:55 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Wallet from './components/Wallet/Wallet'
 import './App.css'
 import Navigation from './components/Navigation/Navigation'
 import DisplayPanel from './components/DisplayPanel/DisplayPanel'
+import TokenApproval from './components/StakeToken/TokenApproval'
+import StakeAmount from './components/StakeToken/StakeAmount'
 
 function App() {
 
