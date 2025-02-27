58bc15ec3eab598a7c2fea010c6630550fcb3be1
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-02-27 22:56:04 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,56 @@
-import Wallet from './components/Wallet/Wallet'
-import './App.css'
-import Navigation from './components/Navigation/Navigation'
-import DisplayPanel from './components/DisplayPanel/DisplayPanel'
+import { useState } from "react"
+import Wallet from "./components/Wallet/Wallet"
+import Navigation from "./components/Navigation/Navigation"
+import DisplayPanel from "./components/DisplayPanel/DisplayPanel"
 import TokenApproval from './components/StakeToken/TokenApproval'
 import StakeAmount from './components/StakeToken/StakeAmount'
-import WithdrawStakeAmount from './components/WithDraw/WithDraw'
-import ClaimReward from './components/ClaimReward/ClaimReward'
-
+import WithdrawStakeAmount from "./components/WithDraw/WithDraw"
+import { StakingProvider } from './context/StakingContext'
+import './App.css'
 function App() {
 
+  const [displaySection, setDisplaySection] = useState("stake");
+
+  const handleButtonClick = (section) => {
+    setDisplaySection(section);
+  };
+
   return (
-    <>
+    <div className="main-section">
       <Wallet>
-        <Navigation/>
-        <DisplayPanel/>
-        <StakeAmount/>
-        <TokenApproval/>
-        <WithdrawStakeAmount/>
-        <ClaimReward/>
+        <Navigation />
+        <StakingProvider>
+          <DisplayPanel />
+          <div className="main-content">
+            <div className="button-section">
+              <button
+                onClick={() => handleButtonClick("stake")}
+                className={displaySection === "stake" ? "" : "active"}
+              >
+                Stake
+              </button>
+              <button
+                onClick={() => handleButtonClick("withdraw")}
+                className={displaySection === "withdraw" ? "" : "active"}
+              >
+                Withdraw
+              </button>
+            </div>
+            {displaySection === "stake" && (
+              <div className="stake-wrapper">
+                <TokenApproval />
+                <StakeAmount />
+              </div>
+            )}
+            {displaySection === "withdraw" && (
+              <div className="stake-wrapper">
+                <WithdrawStakeAmount />
+              </div>
+            )}
+          </div>
+        </StakingProvider>
       </Wallet>
-    </>
+    </div>
   )
 }
 
