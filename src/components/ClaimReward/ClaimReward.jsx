c95fc3dcf096b92de54c98c13a9b27d69cb5d805
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-13 08:42:47 +0530

    Update ClaimReward.jsx

--- src/components/ClaimReward/ClaimReward.jsx
+++ src/components/ClaimReward/ClaimReward.jsx
@@ -1,33 +1,39 @@
-import { useState, useContext } from "react";
+import {  useContext } from "react";
 import Web3Context from "../../context/Web3Context";
 import Button from "../Button/Button";
+import "./ClaimReward.css";
+import {toast} from "react-hot-toast";
 
 const ClaimReward = () => {
     const {stakingContract} = useContext(Web3Context);
-    const [transactionStatus , setTransactionStatus] = useState("");
     const claimReward = async() => {
         try{
             const transaction = await stakingContract.getReward();
-            setTransactionStatus("Transaction is in pending state");
-            const receipt = await transaction.wait();
-            if(receipt.status===1){
-                setTransactionStatus("Transaction is successful");
-                setTimeout(() => {
-                    setTransactionStatus("")
-                } , 5000)
-            }
-            else{
-                setTransactionStatus("Transaction failed");
-            }
+            await toast.promise(transaction.wait(),{
+                loading: "Transaction Pending",
+                success: 'Transaction successful 👌',
+                error: 'Transaction failed 🤯'
+            })
+
+            //if(receipt.status===1){
+            //    setTransactionStatus("Transaction is successful");
+            //    setTimeout(() => {
+            //        setTransactionStatus("")
+            //    } , 5000)
+            //}
+            //else{
+            //    setTransactionStatus("Transaction failed");
+            //}
         }catch(error){
             console.error("Withdraw failed" , error.message);
         }
     }
-    return(
+    return (
         <>
-        {transactionStatus && <div>{transactionStatus}</div>}
-        <Button type = "button" label="Claim Reward" onClick={claimReward} />
+        <div className="claim-reward">
+         <Button type="button" label="Claim Reward" onClick={claimReward}/>
+         </div>
         </>
-    )
+     )
 }
 export default ClaimReward;
